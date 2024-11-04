beaa626ae9b70022d5bbeb45743a1ddaa5587932
jarsnow <raderjohna@gmail.com>
2024-11-03 23:53:33 -0600

    completed lab

--- matrix-template-1.cpp
+++ matrix-template-1.cpp
@@ -11,22 +11,87 @@ private:
 
 public:
   // 1. Read values from stdin into a matrix
-  void readFromStdin();
+  void readFromStdin(){
+    for (int row_i = 0; row_i < SIZE; row_i++){
+      for (int col_i = 0; col_i < SIZE; col_i++){
+        cout << "Enter a value for " << row_i << "," << col_i << ": " << endl;
+        cin >> data[row_i][col_i];
+      }
+    }
+  };
 
   // 2. Display a matrix
-  void display() const;
+  void display() const{
+    for (int row_i = 0; row_i < SIZE; row_i++){
+      for (int col_i = 0; col_i < SIZE; col_i++){
+        cout << data[row_i][col_i] << " ";
+      }
+      cout << endl;
+    };
+  }
 
   // 3. Add two matrices (operator overloading for +)
-  Matrix operator+(const Matrix& other) const;
+  Matrix operator+(const Matrix& other) const{
+    Matrix res;
+
+    for (int row_i = 0; row_i < SIZE; row_i++){
+      for (int col_i = 0; col_i < SIZE; col_i++){
+        // add values
+        res.data[row_i][col_i] = data[row_i][col_i] + other.data[row_i][col_i];
+      }
+    }
+
+    return res;
+  };
 
   // 4. Multiply two matrices (operator overloading for *)
-  Matrix operator*(const Matrix& other) const;
+  Matrix operator*(const Matrix& other) const{
+    Matrix res;
+
+    for (int row_i = 0; row_i < SIZE; row_i++){
+      for (int col_i = 0; col_i < SIZE; col_i++){
+        // get dot product of rows
+        int dot_product = 0;
+        for (int i = 0; i < SIZE; i++){
+          dot_product += data[row_i][i] * other.data[i][col_i];
+        }
+        res.data[row_i][col_i] = dot_product;
+      }
+    }
+
+    return res;
+  };
 
   // 5. Compute the sum of matrix diagonal elements
-  int sumOfDiagonals() const;
+  int sumOfDiagonals() const{
+    int sum = 0;
+    // calculate main diagonal
+    for (int i = 0; i < SIZE; i++){
+      sum += data[i][i];
+    }
+    // calculate secondary diagonal
+    for (int i = 0; i < SIZE; i++){
+      sum += data[i][(SIZE - 1) - i];
+    }
+
+    // subtract the middle element ONLY if it is added twice (size is odd)
+    if (SIZE % 2 == 1){
+      sum -= data[SIZE / 2][SIZE / 2];
+    }
+
+    return sum;
+  };
 
   // 6. Swap matrix rows
-  void swapRows(int row1, int row2);
+  void swapRows(int row1, int row2){
+    for (int i = 0; i < SIZE; i++){
+      // swap values
+      int temp_val = data[row1][i];
+      data[row1][i] = data[row2][i];
+      data[row2][i] = temp_val;
+    }
+  };
+
 };
 
 int main() {
